51383d7fe3b42b06f47a1e31ff6febd8a02aa5ed
kamilamin <kamil.amin@live.co.uk>
2019-01-20 19:25:01 +0500

    Changes in counter-app

--- counter_app/App.js
+++ counter_app/App.js
@@ -1,38 +1,55 @@
-import React, {Component} from 'react';
-import {
-  StyleSheet, 
-  Text, 
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
+} from "react-native";
 
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      value: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
     this.state.customStyle = {
-      color: 'white'
-    }
+      color: "white"
+    };
     setInterval(() => {
-      if(this.state.customStyle.color == 'white'){
+      if (this.state.customStyle.color == "white") {
         this.setState({
           customStyle: {
-            color: 'black'
+            color: "black"
           }
-        })
+        });
       } else {
         this.setState({
           customStyle: {
-            color: 'white'
+            color: "white"
           }
-        })
+        });
       }
     }, 1000);
+  }
+  
+  handleChange() {
+    console.log(this._username, this._password);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.half1}>
+        <Text>Username</Text>
+        <TextInput
+          ref={input => this._username = input}
+        />
+        <Text>Password</Text>
+        <TextInput
+          ref={input => this._password = input}
+        />
+        <Button title="Submit" onPress={this.handleChange} />
+        {/* <View style={styles.half1}>
           <Text>This is half 1/1</Text>
           <Text style={this.state.customStyle}>Hello World</Text>
         </View>
@@ -45,7 +62,7 @@ export default class App extends Component {
             <Text>This is half 2/2</Text>
             <Text style={this.state.customStyle}>Hello World</Text>
           </View>
-        </View>
+        </View> */}
       </View>
     );
   }
@@ -54,27 +71,29 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  half1: {
-    flex: 1,
-    backgroundColor: 'red',
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  half2:{
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'green'
-  },
-  half2x: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  half21: {
-    backgroundColor: 'blue'
-  },
-  half22: {
-    backgroundColor: 'yellow'
+    padding: 20
   }
+  // half1: {
+  //   flex: 1,
+  //   backgroundColor: 'red',
+  //   justifyContent: 'center',
+  //   alignItems: 'center'
+  // },
+  // half2:{
+  //   flex: 1,
+  //   flexDirection: 'row',
+  //   backgroundColor: 'green'
+  // },
+  // half2x: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
+  // half21: {
+  //   backgroundColor: 'blue'
+  // },
+  // half22: {
+  //   backgroundColor: 'yellow'
+  // }
 });
